fb109e8db023fb8235c2986e9c99bd97ece50ac2
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2025-03-28 23:53:41 +0200

    small improvements

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 20:57:20 by hskrzypi          #+#    #+#             */
-/*   Updated: 2025/03/28 21:01:59 by hskrzypi         ###   ########.fr       */
+/*   Updated: 2025/03/28 23:49:19 by hskrzypi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,21 @@ Fixed::Fixed(const Fixed &initial)
 	*this = initial;
 }
 
+Fixed::Fixed(const int valueInt)
+{
+	std::cout << "Int constructor called" << std::endl;
+	int result = valueInt * (1 << bits);//it could just be (value * 256)
+	_value = result;
+}
+
+Fixed::Fixed(const float valueFl)
+{
+	std::cout << "Float constructor called" << std::endl;
+	float temp = valueFl * (1 << bits);//this also could be (value * 256.0f)
+	int rounded = roundf(temp);
+	_value = rounded;
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
@@ -33,7 +48,7 @@ Fixed& Fixed::operator=(const Fixed& initial)
 	std::cout << "Copy assignement operator called" << std::endl;
 	if (this != &initial)
 	{
-		_value = initial.getRawBits();
+		this->_value = initial.getRawBits();
 	}
 	return (*this);
 }
@@ -48,4 +63,20 @@ void	Fixed::setRawBits( int const raw )
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	_value = raw;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return _value / (float)(1 << bits);
+}
+
+int	Fixed::toInt (void ) const
+{
+	return _value >> bits;
+}
+
+std::ostream &operator << (std::ostream &outstream, const Fixed &fixed)
+{
+	outstream << fixed.toFloat();
+	return outstream;
 }
