79bb2f24fcbaf202b8d814b5b84e927832dd0570
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2025-05-25 20:44:29 +0300

    research on casting and pseudo literals

---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 17:44:58 by hskrzypi          #+#    #+#             */
+/*   Updated: 2025/05/25 18:23:47 by hskrzypi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_H
+#define SCALARCONVERTER_H
+#include <string>
+
+class ScalarConverter
+{
+	private:
+		ScalarConverter() = delete;
+		ScalarConverter(const ScalarConverter &original) = delete;
+		ScalarConverter &operator=(ScalarConverter &soriginal) = delete;
+		~ScalarConverter() = delete;
+	public:
+		static void convert(std::string toConvert);
+};
+
+#endif
---
+++ CPP06/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 17:24:46 by hskrzypi          #+#    #+#             */
+/*   Updated: 2025/05/25 17:25:14 by hskrzypi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+//static cast for ex00, reinterpret for ex01, dynamic for ex02
+//nan, inf, -inf - special floating-point values used in numerical computing
+//to represent results of certain undefined or unrepresentable mathematical operations
+
+//nan - not a number, result of undefined or unrepresentable maths
+//eg. dividing zero by zero, multiplying infinity by 0 (inf * 0)
+//square root of negative number, subracting infinity from infinity
+
+//inf (infinity) - represents the result of math op that yields an infinitely large outcome
+//dividing a non-zero number by zeo
+//multiplying infinity by a non-zero
+//adding or substracting infinity to or from a finite number
+//can be positive or negative
