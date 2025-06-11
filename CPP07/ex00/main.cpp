9d7eceac800982f6b833f4c73fa5da724096b296
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2025-06-11 21:45:50 +0300

    missing updates

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -96,4 +96,20 @@ int main(void)
 		float smaller = min<float>(a, b);
 		std::cout << "smaller of the two is... : " << smaller << std::endl;
 	}
+	{
+		std::cout << "--school main--\n";
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	return 0;
 }
