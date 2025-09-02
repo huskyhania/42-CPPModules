e994cd58786b5bcd2e3a46bf7794851bd197904b
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2025-09-02 13:39:46 +0300

    use of steady clock instead of high resolution

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
         if (argc < 2)
             throw std::runtime_error("too few arguments to sort");
         PmergeMe misort;
-        auto timer_start = std::chrono::high_resolution_clock::now();
+        auto timer_start = std::chrono::steady_clock::now();
         for (int i = 1; i < argc; ++i) 
         {
             std::istringstream iss(argv[i]);
@@ -26,14 +26,14 @@ int main(int argc, char **argv)
         misort.sortVector();
         std::cout << "After:  ";
         misort.printVec();
-        auto timer_end = std::chrono::high_resolution_clock::now();
+        auto timer_end = std::chrono::steady_clock::now();
         auto runtime = timer_end - timer_start;
         auto us = std::chrono::duration_cast<std::chrono::microseconds>(runtime).count();
         std::cout << "Time to process a range of " << misort.getVecSize() << " with std::vector : " << us << " µs (microseconds)" << std::endl; 
         std::cout << "\033[31mTotal comparisons: " << misort.getComparisons() << std::endl;
 		misort.resetComparisons();
 		std::cout << " ------ DEQUE -------\n";
-		auto timer_start2 = std::chrono::high_resolution_clock::now();
+		auto timer_start2 = std::chrono::steady_clock::now();
         for (int i = 1; i < argc; ++i) 
         {
             std::istringstream iss(argv[i]);
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
         misort.sortDeque();
         std::cout << "After:  ";
         misort.printDeq();
-        auto timer_end2 = std::chrono::high_resolution_clock::now();
+        auto timer_end2 = std::chrono::steady_clock::now();
         auto runtime2 = timer_end2 - timer_start2;
         auto us2 = std::chrono::duration_cast<std::chrono::microseconds>(runtime2).count();
         std::cout << "Time to process a range of " << misort.getDeqSize() << " with std::deque : " << us2 << " µs (microseconds)" << std::endl; 
